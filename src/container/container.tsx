2119bfec90a217d45c7732ac871541b4ac5ff9f0
Joseph Fehrman <joseph.fehrman@gmail.com>
2021-04-28 15:51:33 -0400

    Creating container placeholder component.

---
+++ src/container/container.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react'
+
+export interface ContainerProps {
+  children?: React.ReactNode | React.ReactNode[]
+}
+
+export const Container = () => {
+  return <>Container</>
+}
