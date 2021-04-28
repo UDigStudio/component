e47d35fcfd7f8251676657c9e01932ef0c624365
Joseph Fehrman <joseph.fehrman@gmail.com>
2021-04-27 21:43:39 -0400

    Adding some spacing so you can read stuff.

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -6,7 +6,7 @@ import 'component/dist/index.css'
 const App = () => {
   const [value, setValue] = useState("Create React Library Example 😄");
   const [selectValue, setSelectValue] = useState<string>();
-  return <>
+  return <div style={{padding: 100}}>
     <ExampleComponent text={value} />
     input: <Text value={value} onChange={(newValue: string) => setValue(newValue)} />
     select: <Select onChange={(newValue: string) => setSelectValue(newValue)} value={selectValue} options={[
@@ -14,7 +14,7 @@ const App = () => {
       { value: 2, label: '2' },
       { value: 3, label: '3', disabled: true }
     ]} placeholder="test" />
-  </>
+  </div>
 }
 
 export default App
